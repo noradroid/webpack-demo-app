5d6a1fea629db0d15f0c0d1aff390db997c91c45
noradroid <75635291+noradroid@users.noreply.github.com>
2023-11-15 15:06:44 +0800

    Webpack now bundles all our code

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,7 @@
-const alertService = new AlertService();
-const componentService = new ComponentService();
+import { inputsAreValid } from "./utils/inputs-are-valid";
+import { parseInputs } from "./utils/parse-inputs";
 
-const run = (alertService, componentService) => {
+export const run = (alertService, componentService) => {
   alertService.hideErrors();
   componentService.onClick(() => {
     alertService.hideErrors();
@@ -16,5 +16,3 @@ const run = (alertService, componentService) => {
     }
   });
 };
-
-run(alertService, componentService);
