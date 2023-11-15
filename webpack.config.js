e86e6ddc644f0e12ac54c566ee7a1177bbcba1be
noradroid <75635291+noradroid@users.noreply.github.com>
2023-11-15 16:18:17 +0800

    Cache busting and using HtmlWebpackPlugin with template file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -19,4 +20,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
